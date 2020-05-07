5dde55eff05bb3ec1ec7ef412036111c2cd7ea3a
pavelnagorny <pavelnagornyj@gmail.com>
2020-05-07 18:19:45 +0300

    initial commit

---
+++ task_1.py
@@ -0,0 +1,12 @@
+a = int(input('Enter value a: '))
+b = int(input("Enter value b: "))
+
+abSum = a + b
+abSubst = a - b
+abDiv = a / b
+abMultip = a * b
+
+print("Summ: ", abSum)
+print("Substraction: ", abSubst)
+print("Multiplication: ", abMultip)
+print("Division: ", abDiv)
---
+++ task_2.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+city = input("Where do you live? ")
+
+print("Hello, %s!\nYour age is  %s.\nYou live in %s." % (name,age,city))
